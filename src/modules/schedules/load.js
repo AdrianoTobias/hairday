12cbaa27e175dfb856ac6f2f88d37c084eb3488d
Adriano <adrianoact@msn.com>
2024-06-20 20:42:57 -0300

    add show schedules rendering

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "./show.js";
 
 // Seleciona o input de data:
 const selectedDate = document.getElementById("date");
@@ -10,7 +11,9 @@ export async function schedulesDay() {
 
     // Busca na API os agendamentos do dia:
     const dailySchedules = await scheduleFetchByDay({ date });
-    console.log(dailySchedules);
+    
+    // Renderiza os agendamentos:
+    schedulesShow({ dailySchedules });
 
     // Renderiza os horários disponíveis:
     hoursLoad({ date })
