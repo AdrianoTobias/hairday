b136a9df03fdb7d62aa11a8292ff27f45fdaa70b
Adriano <adrianoact@msn.com>
2024-06-20 22:20:00 -0300

    add reload schedules day and reset input name on submit form

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js"
 
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
@@ -40,11 +41,18 @@ form.onsubmit = async (event) => {
         // Gera um ID:
         const id = new Date().getTime();
 
+        // Faz o agendamento:
         await scheduleNew({
             id,
             name,
             when
         });
+
+        // Revarrega os agendamentos:
+        await schedulesDay();
+
+        // Limpa o input de nome do cliente:
+        clientName.value = "";
     } catch (error) {
         
     }
