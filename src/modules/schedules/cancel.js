ba5bf2cb71702959c5add4ad67ac82c81ab482da
Adriano <adrianoact@msn.com>
2024-06-20 23:47:48 -0300

    add cancel schedule service

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,19 +1,31 @@
+import { schedulesDay } from "./load.js"
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll(".period");
 
 // Gera evento click para cada agendamento:
 periods.forEach(period => {
     // Captura o evento de clique no agendamento:
-    period.addEventListener("click", event => {
+    period.addEventListener("click", async event => {
         if (event.target.classList.contains("cancel-icon")) {
             // Obtém a "li" pai do elemento clicado:
             const item = event.target.closest("li");
-            const { id } = item.dataset;
 
+            // Obtém o id (data-id) do agendamento para remover:
+            const { id } = item.dataset;
+            
+            // Verifica se há um id:
             if (id) {
+                // Confirma se o usuário quer mesmo cancelar:
                 const isConfirm = confirm("Tem certeza que deseja cancelar o agenamento?");
 
-                if (isConfirm)
-                    console.log("Remover!")
+                if (isConfirm) {
+                    // Faz requisição na API para cancelar o agendamento:
+                    await scheduleCancel({ id });
+
+                    // Recarrega os agendamentos:
+                    await schedulesDay();
+                }
             }
         }
     })
